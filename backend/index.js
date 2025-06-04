aa0d99d5d13a6458783ea3ce278b9e82ad222fce
Steven Hutchison <stevenhutchisonkc@gmail.com>
2025-06-03 22:46:47 -0500

    Refactor backend for decoupled server. remove /api from endpoints

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,11 @@ import 'dotenv/config';
 import cors from 'cors';
 import path from 'path';
 import yaml from 'yamljs';
-import { fileURLToPath } from 'url';
 import connectMongoose from './utils/mongo.js';
 import ConceptRouter from './routes/concept.router.js';
 import BookRouter from './routes/book.router.js';
 import AdminRouter from './routes/admin.router.js';
 import swaggerUi from 'swagger-ui-express';
-
-
-// Setup __dirname in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Constants
 const PORT = process.env.PORT || 3000;
@@ -36,17 +30,9 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocument));
 // app.use('/api/admin', AdminRouter);
 
 // Connect router to concepts endpoint
-app.use('/api/concepts', ConceptRouter);
+app.use('/concepts', ConceptRouter);
 
-app.use('/api/books', BookRouter)
-
-// Serve static Vue files
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Fallback for SPA routing
-app.get('/*path', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+app.use('/books', BookRouter)
 
 app.listen(PORT, ()=>{
   console.log(`Server is listening on port ${PORT}`)
