19f4e7cc4006e184d52d1ee45fe527b9521127d9
Steven Hutchison <stevenhutchisonkc@gmail.com>
2025-06-03 23:18:04 -0500

    Remove all but index.js from api into src directory

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,20 +1,24 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
-import path from 'path';
 import yaml from 'yamljs';
-import connectMongoose from './utils/mongo.js';
-import ConceptRouter from './routes/concept.router.js';
-import BookRouter from './routes/book.router.js';
-import AdminRouter from './routes/admin.router.js';
+import connectMongoose from '../src/utils/mongo.js';
+import ConceptRouter from '../src/routes/concept.router.js';
+import BookRouter from '../src/routes/book.router.js';
+import AdminRouter from '../src/routes/admin.router.js';
 import swaggerUi from 'swagger-ui-express';
-import { admin_check } from './middleware/admin_check.js';
+import { admin_check } from '../src/middleware/admin_check.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Constants
 const PORT = process.env.PORT || 3000;
 
 // Load your OpenAPI spec
-const openApiDocument = yaml.load(path.join(process.cwd(), 'docs/openapi.yaml'));
+const openApiDocument = yaml.load(path.join(__dirname, 'docs/openapi.yaml'));
 
 // Create Express app
 const app = express();
